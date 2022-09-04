3599223fbd95c1d9be1cd89e4c14bd5731c597c2
qpakr38 <zczc0819@gmail.com>
2022-09-04 14:53:04 +0900

    FEAT : Http 오류 처리하기

--- src/App.js
+++ src/App.js
@@ -5,19 +5,45 @@ import './App.css';
 
 function App() {
     const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState({isError: false, message: ''});
 
     async function fetchMovieHandler() {
-        const response = await fetch('https://swapi.dev/api/films')
-        const data = await response.json();
-        const transformedMovies = data.results.map((movieData) => {
-            return {
-                id: movieData.episode_id,
-                title: movieData.title,
-                openingText: movieData.opening_crawl,
-                releaseDate: movieData.release_date
+        setIsLoading(true);
+        setError({isError: false, message: ''});
+        try {
+            const response = await fetch('https://swapi.dev/api/films');
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
             }
-        });
-        setMovies(transformedMovies);
+            const data = await response.json();
+            const transformedMovies = data.results.map((movieData) => {
+                return {
+                    id: movieData.episode_id,
+                    title: movieData.title,
+                    openingText: movieData.opening_crawl,
+                    releaseDate: movieData.release_date
+                }
+            });
+            setMovies(transformedMovies);
+        } catch (e) {
+            console.log(e);
+            setError({isError: true, message: "통신 오류가 발생하였습니다."});
+        }
+        setIsLoading(false);
+
+    }
+
+    let contents = <p>Found no movies.</p>;
+
+    if (movies.length > 0) {
+        contents = <MoviesList movies={movies}/>
+    }
+    if (error.isError) {
+        contents = <p>{error.message}</p>;
+    }
+    if (isLoading) {
+        contents = <p>Loading...</p>;
     }
 
     return (
@@ -25,9 +51,7 @@ function App() {
             <section>
                 <button onClick={fetchMovieHandler}>Fetch Movies</button>
             </section>
-            <section>
-                <MoviesList movies={movies}/>
-            </section>
+            <section>{contents}</section>
         </React.Fragment>
     );
 }
